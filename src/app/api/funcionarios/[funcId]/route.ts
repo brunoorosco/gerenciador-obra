84dbe97683bb39c0fe7868519b456397b4cb02a9
Bruno Orosco <40275604+brunoorosco@users.noreply.github.com>
2023-10-16 12:47:35 -0300

    fix: ajustes api

--- src/app/api/funcionarios/[funcId]/route.ts
+++ src/app/api/funcionarios/[funcId]/route.ts
@@ -1,12 +1,11 @@
-import type { NextApiRequest } from 'next'
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET(req: NextApiRequest, { params, query }: any) {
-	const funcId = params.funcId
-	console.log(req.query)
-	console.log(params)
-	const result = await fetch(`https://swapi.dev/api/people/${funcId}`)
-	if (!result.ok) throw new Error('Problemas com o Fetch')
-	const data = await result.json()
-	return NextResponse.json({ data })
+export async function GET(req: NextRequest, { params, query }: any) {
+  const funcId = params.funcId
+  console.log(req)
+  console.log(params)
+  const result = await fetch(`https://swapi.dev/api/people/${funcId}`)
+  if (!result.ok) throw new Error('Problemas com o Fetch')
+  const data = await result.json()
+  return NextResponse.json({ data })
 }
--- src/app/api/funcionarios/route.ts
+++ src/app/api/funcionarios/route.ts
@@ -1,8 +1,7 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import { accessGoogle } from '@/infra/google/accessGoogle'
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest, res: NextResponse) {
   const ss = await accessGoogle()
   let sheet = ss.sheetsById[1679707489]
 
@@ -18,7 +17,7 @@ export async function GET(req: NextApiRequest, res: NextApiResponse) {
   return NextResponse.json(datas)
 }
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+export async function POST(req: NextRequest, res: NextResponse) {
   const result = await fetch(`https://swapi.dev/api/people`)
   if (!result.ok) throw new Error('Problrmas com o Fetch')
   const data = await result.json()
